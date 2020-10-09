bd6eb22a7c2373eeb13579275db21bdcd292b0a1
Cai Nowicki <cai.nowicki@gmail.com>
2020-10-09 00:06:33 -0400

    first pass at rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 
 import sys
+from itertools import permutations
+
 
 def rock_paper_scissors(n):
-  # Your code here
+    possible_moves = ["rock", "paper", "scissors"]
+    possible_rounds = [list(x) for x in permutations(possible_moves,n)]
+    return possible_rounds
 
-  pass
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
